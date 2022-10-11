a48b0fb78dddbcdc1a2ac17a9cc17c279ad1119d
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-10-11 13:07:11 +0300

    Solved

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        t = Math.abs(t);
+        String number = String.valueOf(t);
+        int sum = 0;
+        for (int i = 0; i < number.length(); i++){
+            sum += Integer.parseInt(String.valueOf(number.charAt(i)));
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        StringBuilder fib = new StringBuilder("01");
+        for(int i = 0; i < lastFibonacci - 2; i++){
+            fib.append(Integer.parseInt(String.valueOf(fib.charAt(i))) + Integer.parseInt(String.valueOf(fib.charAt(i + 1))));
+        }
+        for(String s : fib.toString().split("")){
+            System.out.println(s);
+        }
     }
 }
